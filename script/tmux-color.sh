b558e41aa7c6e4b28ad999f850fbb2a840d17777
caivy <samnangkhath5@gmail.com>
2024-05-29 13:03:50 +0700

    Fix Tmux "Bad Colors" correctly sourcing pywal colors

--- script/tmux-color.sh
+++ script/tmux-color.sh
@@ -3,22 +3,27 @@
 # Path to colors-kitty.conf
 COLOR_FILE="$HOME/.cache/wal/colors-kitty.conf"
 
+# Function to get color value from the file
+get_color() {
+    grep "^$1" "$COLOR_FILE" | awk '{print $2}'
+}
+
 # Read colors from the file and export as environment variables
-export TMUX_COLOR_FOREGROUND=$(grep '^foreground' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_BACKGROUND=$(grep '^background' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR0=$(grep '^color0' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR1=$(grep '^color1' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR2=$(grep '^color2' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR3=$(grep '^color3' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR4=$(grep '^color4' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR5=$(grep '^color5' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR6=$(grep '^color6' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR7=$(grep '^color7' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR8=$(grep '^color8' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR9=$(grep '^color9' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR10=$(grep '^color10' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR11=$(grep '^color11' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR12=$(grep '^color12' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR13=$(grep '^color13' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR14=$(grep '^color14' $COLOR_FILE | cut -d ' ' -f 2)
-export TMUX_COLOR_COLOR15=$(grep '^color15' $COLOR_FILE | cut -d ' ' -f 2)
+export TMUX_COLOR_FOREGROUND=$(get_color 'foreground')
+export TMUX_COLOR_BACKGROUND=$(get_color 'background')
+export TMUX_COLOR_COLOR0=$(get_color 'color0')
+export TMUX_COLOR_COLOR1=$(get_color 'color1')
+export TMUX_COLOR_COLOR2=$(get_color 'color2')
+export TMUX_COLOR_COLOR3=$(get_color 'color3')
+export TMUX_COLOR_COLOR4=$(get_color 'color4')
+export TMUX_COLOR_COLOR5=$(get_color 'color5')
+export TMUX_COLOR_COLOR6=$(get_color 'color6')
+export TMUX_COLOR_COLOR7=$(get_color 'color7')
+export TMUX_COLOR_COLOR8=$(get_color 'color8')
+export TMUX_COLOR_COLOR9=$(get_color 'color9')
+export TMUX_COLOR_COLOR10=$(get_color 'color10')
+export TMUX_COLOR_COLOR11=$(get_color 'color11')
+export TMUX_COLOR_COLOR12=$(get_color 'color12')
+export TMUX_COLOR_COLOR13=$(get_color 'color13')
+export TMUX_COLOR_COLOR14=$(get_color 'color14')
+export TMUX_COLOR_COLOR15=$(get_color 'color15')
